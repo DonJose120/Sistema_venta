464663c7a3a3117c9ba616c10e2c2f0f141fef42
Donjose120 <yuturoha497@gmail.com>
2023-07-17 18:01:41 -0500

    actualizacion

--- database/migrations/2023_07_17_222043_create_cliente_table.php
+++ database/migrations/2023_07_17_222043_create_cliente_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('cliente', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name', 50);
+            $table->string('last name', 50);
+            $table->string('address',40);
+            $table->string('telefono',14);
+            $table->string('correo',30);
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('cliente');
     }
 };
